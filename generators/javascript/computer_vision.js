49456c3a1aecfe14d8c938621604f6605634e501
Christian Frisson <christian.frisson@gmail.com>
2017-03-21 15:26:52 +0100

    One-line generation of JavaScript for extract_test

--- generators/javascript/computer_vision.js
+++ generators/javascript/computer_vision.js
@@ -1,13 +1,13 @@
 'use strict';
 goog.provide('Blockly.JavaScript.InspectorWidget.ComputerVision');
 //goog.require('Blockly.InspectorWidget.JavaScript');
-Blockly.JavaScript['match_template'] = function (block) {
+Blockly.JavaScript['match_template'] = function(block) {
     // Template setter.
     var argument0 = Blockly.JavaScript.valueToCode(block, 'TEMPLATE', Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
     var varName = Blockly.JavaScript.templateDB_.getName(block.getFieldValue('TEMPLATE'), Blockly.Templates.NAME_TYPE);
     return 'matchTemplate(' + varName + ');\n';
 };
-Blockly.JavaScript['controls_if'] = function (block) {
+Blockly.JavaScript['controls_if'] = function(block) {
     // If/elseif/else condition.
     var n = 0;
     var argument = Blockly.JavaScript.valueToCode(block, 'IF' + n, Blockly.JavaScript.ORDER_NONE) || 'false';
@@ -24,7 +24,7 @@ Blockly.JavaScript['controls_if'] = function (block) {
     }
     return code;
 };
-Blockly.JavaScript['extract_test'] = function (block) {
+Blockly.JavaScript['extract_test'] = function(block) {
     var isIf = block.getFieldValue('MODE') == 'IF';
     var isBetween = block.getFieldValue('MODE') == 'BETWEEN';
     var isAbove = block.getFieldValue('MODE') == 'ABOVE';
@@ -37,17 +37,13 @@ Blockly.JavaScript['extract_test'] = function (block) {
     var test = block.getFieldValue('MODE');
     if (test === 'IF') {
         argument1 = Blockly.JavaScript.valueToCode(block, 'TEST_VALUE', /*until ? Blockly.JavaScript.ORDER_LOGICAL_NOT :*/ Blockly.JavaScript.ORDER_NONE) || 'false';
-    }
-    else if (test === 'BETWEEN') {
+    } else if (test === 'BETWEEN') {
         argument1 = block.getFieldValue('TEMPLATE1') + ',' + block.getFieldValue('TEMPLATE2');
-    }
-    else if (test === 'ABOVE' || test === 'BELOW' || test === 'LEFTOF' || test === 'RIGHTOF') {
+    } else if (test === 'ABOVE' || test === 'BELOW' || test === 'LEFTOF' || test === 'RIGHTOF') {
         argument1 = block.getFieldValue('TEMPLATE');
-    }
-    else if (test === 'INRECT') {
+    } else if (test === 'INRECT') {
         argument1 = block.getFieldValue('X') + ',' + block.getFieldValue('Y') + ',' + block.getFieldValue('W') + ',' + block.getFieldValue('H');
-    }
-    else {
+    } else {
         // TODO: better handle error here
         console.log('Error for block extract_test: JavaScript export for test ' + test + ' unhandled.');
     }
@@ -56,28 +52,22 @@ Blockly.JavaScript['extract_test'] = function (block) {
     var name = '';
     if (isIf) {
         name = 'if';
-    }
-    else if (isBetween) {
+    } else if (isBetween) {
         name = 'between';
-    }
-    else if (isAbove) {
+    } else if (isAbove) {
         name = 'above';
-    }
-    else if (isBelow) {
+    } else if (isBelow) {
         name = 'below';
-    }
-    else if (isLeftOf) {
+    } else if (isLeftOf) {
         name = 'leftof';
-    }
-    else if (isRightOf) {
+    } else if (isRightOf) {
         name = 'rightof';
-    }
-    else if (isInRect) {
+    } else if (isInRect) {
         name = 'inrect';
     }
-    return name + '(' + argument1 + ') {' + branch + '}\n';
+    return name + '(' + argument1 + ') {' + branch.toString().split('\n').join('') + '}\n';
 };
-Blockly.JavaScript['detect_alphanum'] = function (block) {
+Blockly.JavaScript['detect_alphanum'] = function(block) {
     var argument0 = Blockly.JavaScript.valueToCode(block, 'TEST_VALUE', /*until ? Blockly.JavaScript.ORDER_LOGICAL_NOT :*/ Blockly.JavaScript.ORDER_NONE) || 'false';
     var argument1; //= block.getFieldValue('TEMPLATE_NAME');
     var test = block.getFieldValue('MODE');
@@ -85,21 +75,17 @@ Blockly.JavaScript['detect_alphanum'] = function (block) {
     // Replace by function finding name in DETECT_TYPES 
     if (test === 'TEXT') {
         name = "detectText";
-    }
-    else if (test === 'NUMBER') {
+    } else if (test === 'NUMBER') {
         name = "detectNumber";
-    }
-    else if (test === 'TIME') {
+    } else if (test === 'TIME') {
         name = "detectTime";
-    }
-    else {
+    } else {
         // TODO: better handle error here
         console.log('Error for block detect_alphanum: JavaScript export for type ' + test + ' unhandled.');
     }
     if (test === 'TEXT' || test === 'NUMBER' || test === 'TIME') {
         argument1 = block.getFieldValue('TEMPLATE');
-    }
-    else {
+    } else {
         // TODO: better handle error here
         console.log('Error for block detect_alphanum: JavaScript export for type ' + test + ' unhandled.');
     }
